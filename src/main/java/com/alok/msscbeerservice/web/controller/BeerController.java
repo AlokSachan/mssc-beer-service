5599a187efc5f0752373c4181c79ddc7a6efc286
AlokSachan <alok.sachan.83@gmail.com>
2020-07-05 12:18:58 +0530

    adding beer entity.  #5

--- src/main/java/com/alok/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/alok/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,7 @@
 package com.alok.msscbeerservice.web.controller;
 
 import com.alok.msscbeerservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +10,17 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/beer")
+@Slf4j
 public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId")UUID beerId){
-
+        log.info("Get is calling");
         return new ResponseEntity<>(BeerDto.builder().build(),HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
-
+ log.info("Post is calling");
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
@@ -26,6 +28,7 @@ public class BeerController {
     public ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId,
                                          @RequestBody BeerDto beerDto){
 
+        log.info("Update is calling  ");
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
