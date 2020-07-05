33b3cd04002f35f06bddf2cfed93f85088e735ec
AlokSachan <alok.sachan.83@gmail.com>
2020-07-05 19:49:07 +0530

    Added validation. #8

--- src/main/java/com/alok/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/alok/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -17,17 +18,28 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BeerDto {
-    private UUID id;
-    private Integer version;
 
+    @Null
+    private UUID id;
+    @Null
+    private Integer version;
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
     private Integer quantityOnHold;
 }
