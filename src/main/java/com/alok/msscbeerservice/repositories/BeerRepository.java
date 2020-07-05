aec7121cbcfa0b9d119d6db983147ecb868eb2b2
AlokSachan <alok.sachan.83@gmail.com>
2020-07-05 12:25:35 +0530

    adding beer repository.  #6

---
+++ src/main/java/com/alok/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.alok.msscbeerservice.repositories;
+
+import com.alok.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
