7ef235e8be54d4c466f0e64947499be830bf70a7
AlokSachan <alok.sachan.83@gmail.com>
2020-07-05 20:01:28 +0530

    Added error handler. #9

--- src/main/java/com/alok/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/alok/msscbeerservice/web/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.alok.msscbeerservice.web.model.BeerDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,14 +20,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
  log.info("Post is calling");
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId,
-                                         @RequestBody BeerDto beerDto){
+                                         @RequestBody @Validated BeerDto beerDto){
 
         log.info("Update is calling  ");
         return new ResponseEntity(HttpStatus.NO_CONTENT);
